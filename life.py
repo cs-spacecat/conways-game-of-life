4a447e785125f5c7dd8471b6cfb7c895477736cf
Spacecat <69915180+cs-spacecat@users.noreply.github.com>
2024-03-22 13:48:32 +0100

    added black background to stepcounter

--- life.py
+++ life.py
@@ -58,13 +58,16 @@ def drawField() -> None:
 
 def zoom(scrollDelta: int) -> None:
     global cellSize, cameraPos
+    # limiting the zoom
+    if scrollDelta * 2 + cellSize <= 0 or scrollDelta * 2 + cellSize >= 200: return 
     mPos = pygame.mouse.get_pos()
     oldRelPos = pixelPos2relPosEx(mPos)
     cellSize += scrollDelta * 2
     newRelPos = pixelPos2relPosEx(mPos)
     cameraPos = (
         (newRelPos[0] - oldRelPos[0]) * cellSize + cameraPos[0],
-        (newRelPos[1] - oldRelPos[1]) * cellSize + cameraPos[1])
+        (newRelPos[1] - oldRelPos[1]) * cellSize + cameraPos[1]
+    )
 
 def configHandling(filename: str = "config.ini", conf: list = []) -> None:
     global steps, cameraPos, cellSize, panSpeed, field, screenSize, fieldSize, simulationSpeed, simulationSpeed
@@ -95,10 +98,12 @@ def listNeighbors(x: int, y: int, tempfield) -> int:
     return alive - tempfield[y][x]
 
 def display_steps():
-    # font = pygame.font.Font("freesansbold", 45)
     font = pygame.font.Font(pygame.font.get_default_font(), 30)
     text = font.render(f"Schritte: {steps}", True, "white")
     rect = text.get_rect(topleft=(10, 10))
+    bg = pygame.Surface(text.get_size())
+    bg.fill((0, 0, 0))
+    screen.blit(bg, rect)
     screen.blit(text, rect)
 
 
@@ -122,7 +127,6 @@ def advanceGeneration() -> None:
                 if neighbors == 3:
                     field[y][x] = 1
     steps += 1
-    print(steps)
 
 
 def main() -> None:
